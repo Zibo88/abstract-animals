2b9ac9b9f19428f05cc139f3df6e52cd6493f149
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-02-01 17:19:55 +0100

    creo un metodo all'interno del Main che richiama un metodo della classe

--- src/org/lessons/animals/Main.java
+++ src/org/lessons/animals/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		
 		Scanner data = new Scanner(System.in);
 		
+		
+		
 		String cane = "cane";
 		String delfino = "delfino";
 		String passerotto = "passerotto";
@@ -24,47 +26,45 @@ public class Main {
 //				Cane
 				Cane dog1 = new Cane();
 				
-				System.out.println("\nil" + " " + userChoice +" "+ "quando dorme:");
+				System.out.println("Il cane quando dorme:");
 				dog1.dormi();	
 				
-				System.out.println("Il" + " " + userChoice + " " + "mangia:");
+				System.out.println("Il cane mangia:");
 				dog1.mangia();
 				
-				System.out.println("Il verso dell':"+ " " + userChoice + " " + "è");
+				System.out.println("Il verso del cane:");
 				dog1.verso();
 				
-				dog1.faiNuotare(userChoice);
+				faiNuotare(dog1);
+			
 			}else if(userChoice.equals(passerotto)) {
 //				Passerotto
 				Passerotto passerotto1 = new Passerotto();
 				
-				System.out.println("\nl" + " " + userChoice +" "+ "quando dorme:");
+				System.out.println("\nIl passerotto quando dorme:");
 				passerotto1.dormi();	
 				
-				System.out.println("Il" + " " + userChoice + " " + "mangia:");
+				System.out.println("Il passerotto mangia:");
 				passerotto1.mangia();
 				
 				System.out.println("Il verso dell':"+ " " + userChoice + " " + "è");
 				passerotto1.verso();
-				
-				passerotto1.faiVolare(userChoice);
-				
-				
+				faiVolare(passerotto1);
+			
 			}else if(userChoice.equals(aquila)){
 //				Aquila
 				Aquila aquila1 = new Aquila();
-				System.out.println("\nl" + " " + userChoice +" "+ "quando dorme:");
+				System.out.println("\nl" + " " +  userChoice +" "+ "quando dorme:");
 				aquila1.dormi();	
 				
-				System.out.println("Il" + " " + userChoice + " " + "mangia:");
+				System.out.println("l' aquila mangia:");
 				aquila1.mangia();
 				
 				System.out.println("Il verso dell':"+ " " + userChoice + " " + "è");
 				aquila1.verso();
+				faiVolare(aquila1);
 				
-				aquila1.faiVolare(userChoice);
-				
-				
+		
 			}else if(userChoice.equals(delfino)) {
 //				Delfino
 				Delfino delfino1 = new Delfino();
@@ -77,27 +77,23 @@ public class Main {
 				System.out.println("Il verso del:" +" " + userChoice + " " + "è");
 				delfino1.verso();
 				
-				delfino1.faiNuotare(userChoice);
-				
-				
+				faiNuotare(delfino1);
+
 			}else {
 				System.out.println("Questo animale non appartiene alla nostra fattoria");
 			}
 
-			
-			
 			data.close();
 			
 		}
-		
-		
-
-		
-
-		
-		
-
-		
-		
-
+	
+//	creo un metodo che richiama un metodo presente nella classe 
+	public static void faiVolare(Volare volanti){
+		volanti.vola();
+	}
+	
+	public static void faiNuotare(Nuotare nuotanti){
+		nuotanti.nuota();
+	}
 }
+
