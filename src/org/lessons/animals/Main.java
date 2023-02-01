e93e7ce38550b16de213c4a60c967d8e8f39136d
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-02-01 14:50:24 +0100

    creata una interfaccia volare e una nuotare

--- src/org/lessons/animals/Main.java
+++ src/org/lessons/animals/Main.java
@@ -1,65 +1,97 @@
 package org.lessons.animals;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		Cane
-		Cane dog1 = new Cane();
+		Scanner data = new Scanner(System.in);
 		
-		System.out.println("Il cane quando dorme:");
-		dog1.dormi();	
+		String cane = "cane";
+		String delfino = "delfino";
+		String passerotto = "passerotto";
+		String aquila = "aquila";
+		String userChoice;
 		
-		System.out.println("Il cane mangia:");
-		dog1.mangia();
-		
-		System.out.println("Il verso del cane:");
-		dog1.verso();
-		
-//		Passerotto
-		Passerotto passerotto1 = new Passerotto();
-		
-		System.out.println("\nIl passerotto quando dorme:");
-		passerotto1.dormi();	
-		
-		System.out.println("Il passerotto mangia:");
-		passerotto1.mangia();
-		
-		System.out.println("Il verso del passerotto:");
-		passerotto1.verso();
-		
-		passerotto1.faiVolare("passerotto");
+		System.out.println("Ciao, benvenuto nell'allegra fattoria. Digita il nome di un animale per sapere di più");
+		userChoice = data.nextLine().toLowerCase().trim();
 		
 		
-//		Aquila
-		Aquila aquila1 = new Aquila();
-		System.out.println("\nl' aquila quando dorme:");
-		aquila1.dormi();	
+	
+			if(userChoice.equals(cane)) {
+//				Cane
+				Cane dog1 = new Cane();
+				
+				System.out.println("Il cane quando dorme:");
+				dog1.dormi();	
+				
+				System.out.println("Il cane mangia:");
+				dog1.mangia();
+				
+				System.out.println("Il verso del cane:");
+				dog1.verso();
+			}else if(userChoice.equals(passerotto)) {
+//				Passerotto
+				Passerotto passerotto1 = new Passerotto();
+				
+				System.out.println("\nIl passerotto quando dorme:");
+				passerotto1.dormi();	
+				
+				System.out.println("Il passerotto mangia:");
+				passerotto1.mangia();
+				
+				System.out.println("Il verso del passerotto:");
+				passerotto1.verso();
+				
+				passerotto1.faiVolare(passerotto);
+			}else if(userChoice.equals(aquila)){
+//				Aquila
+				Aquila aquila1 = new Aquila();
+				System.out.println("\nl' aquila quando dorme:");
+				aquila1.dormi();	
+				
+				System.out.println("l' aquila mangia:");
+				aquila1.mangia();
+				
+				System.out.println("Il verso dell' aquila:");
+				aquila1.verso();
+				
+				aquila1.faiVolare(aquila);
+			}else if(userChoice.equals(delfino)) {
+//				Delfino
+				Delfino delfino1 = new Delfino();
+				System.out.println("\nil delfino quando dorme:");
+				delfino1.dormi();	
+				
+				System.out.println("Il delfino mangia:");
+				delfino1.mangia();
+				
+				System.out.println("Il verso del delfino:");
+				delfino1.verso();
+				
+				delfino1.faiNuotare(delfino);
+				
+				
+			}else {
+				System.out.println("Questo animale non appartiene alla nostra fattoria");
+			}
+
+			
+			
+			data.close();
+			
+		}
 		
-		System.out.println("l' aquila mangia:");
-		aquila1.mangia();
 		
-		System.out.println("Il verso dell' aquila:");
-		aquila1.verso();
+
 		
-		aquila1.faiVolare("aquila");
+
 		
 		
-//		Delfino
-		Delfino delfino1 = new Delfino();
-		System.out.println("\nil delfino quando dorme:");
-		delfino1.dormi();	
-		
-		System.out.println("Il delfino mangia:");
-		delfino1.mangia();
-		
-		System.out.println("Il verso del delfino:");
-		delfino1.verso();
-		
-		delfino1.faiNuotare("delfino");
+
 		
 		
-	}
 
 }
