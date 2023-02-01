284a1452951642bd90fd13e3260b267523e81a83
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-02-01 16:11:20 +0100

    richiamo il metodo vola/nuota all'interno del metodo faiVolare/faiNuotare

--- src/org/lessons/animals/Passerotto.java
+++ src/org/lessons/animals/Passerotto.java
@@ -8,14 +8,21 @@ public class Passerotto extends Animale implements Volare {
 		System.out.println("cip cip");
 	}
 
-
 	public void mangia() {
 		// TODO Auto-generated method stub
 		System.out.println("mais");
 	}
 	
+//	richiamo un metodo della stessa classe
 	public void faiVolare(String animale) {
-		System.out.println("questo animale vola");
+		if(animale.equals(animale)){
+			this.vola();
+		}
+	}
+	
+	public void vola() {
+			System.out.println("sto volando");
+		
 	}
 	
 	
